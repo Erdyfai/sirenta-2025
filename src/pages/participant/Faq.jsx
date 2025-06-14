a6bf5154c2cab4fd2b038970eadfd295b5b550a2
atroktan <atikarohmah2004@gmail.com>
2025-06-14 19:40:45 +0700

    updatepink

--- src/pages/participant/Faq.jsx
+++ src/pages/participant/Faq.jsx
@@ -1,19 +1,36 @@
+import { useEffect } from 'react';
 import Accordion from '../../components/Accordion';
 import Navbar from '../../components/Navbar';
-import faqData from '../../mockdata/faqdata';
+import { useFaqStore } from '../../stores/useFaqStores';
 
 export default function Faq() {
+  const { faqList, isLoadingFaq, fetchFaq } = useFaqStore();
+
+  useEffect(() => {
+    fetchFaq();
+  }, []);
+
   return (
     <div>
-      <Navbar  />
+      <Navbar />
       <div className="min-h-screen w-[90%] mx-auto pt-28">
-        
-        <div className=" pb-6">
-          <h4 className="font-semibold text-center">FAQ</h4>
+        <div className="pb-6">
+          <h4 className="font-semibold text-center text-2xl">FAQ</h4>
         </div>
-        {faqData.map((faq) => {
-          return <Accordion key={faq.id} question={faq.question} answer={faq.answer} />;
-        })}
+
+        {isLoadingFaq ? (
+          <p className="text-center text-gray-500">Memuat data FAQ...</p>
+        ) : faqList.length === 0 ? (
+          <p className="text-center text-gray-500">Belum ada data FAQ.</p>
+        ) : (
+          faqList.map((faq) => (
+            <Accordion
+              key={faq.id}
+              question={faq.pertanyaan}
+              answer={faq.answer}
+            />
+          ))
+        )}
       </div>
     </div>
   );
